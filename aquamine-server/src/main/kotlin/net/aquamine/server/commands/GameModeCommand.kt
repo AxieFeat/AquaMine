1fafafcfebcd21c6abb06e1f22535c40de64cc36
AxieFeat <kirill@arial.su>
2025-09-06 21:25:14 +1000

    Refactor gamemode command

--- aquamine-server/src/main/kotlin/net/aquamine/server/commands/GameModeCommand.kt
+++ aquamine-server/src/main/kotlin/net/aquamine/server/commands/GameModeCommand.kt
@@ -1,7 +1,6 @@
 package net.aquamine.server.commands
 
 import com.mojang.brigadier.CommandDispatcher
-import net.kyori.adventure.text.Component
 import net.aquamine.api.command.argument
 import net.aquamine.api.command.literalCommand
 import net.aquamine.api.world.GameMode
@@ -10,7 +9,6 @@ import net.aquamine.server.command.arguments.GameModeArgument
 import net.aquamine.server.command.arguments.entities.EntityArgumentType
 import net.aquamine.server.entity.player.AquaPlayer
 import net.aquamine.server.locale.CommandMessages
-import net.aquamine.server.util.enumhelper.GameModes
 import net.aquamine.server.world.rule.GameRuleKeys
 
 object GameModeCommand {
@@ -20,39 +18,30 @@ object GameModeCommand {
 
     @JvmStatic
     fun register(dispatcher: CommandDispatcher<CommandSourceStack>) {
-        val command = literalCommand("gamemode") { requiresPermission(AquaPermission.GAME_MODE) }
-        GameModes.VALUES.forEach { mode ->
-            command.then(literalCommand(mode.name.lowercase()) {
-                runs { setMode(it.source, listOf(it.source.getPlayerOrError()), mode) }
-                argument(TARGETS, EntityArgumentType.players()) {
-                    runs { setMode(it.source, EntityArgumentType.getPlayers(it, TARGETS), mode) }
-                }
-            })
-        }
+        dispatcher.register(literalCommand("gamemode") {
+            requiresPermission(AquaPermission.GAME_MODE)
 
-        command.argument(GAME_MODE, GameModeArgument) {
-            runs { setMode(it.source, listOf(it.source.getPlayerOrError()), GameModeArgument.get(it, GAME_MODE)) }
-            argument(TARGETS, EntityArgumentType.players()) {
-                runs {
-                    setMode(it.source,
-                        EntityArgumentType.getPlayers(it, TARGETS),
-                        GameModeArgument.get(it, GAME_MODE)
-                    )
+            argument(GAME_MODE, GameModeArgument) {
+                runs { setGameMode(it.source, listOf(it.source.getPlayerOrError()), GameModeArgument.get(it, GAME_MODE)) }
+                argument(TARGETS, EntityArgumentType.players()) {
+                    runs {
+                        setGameMode(it.source,
+                            EntityArgumentType.getPlayers(it, TARGETS),
+                            GameModeArgument.get(it, GAME_MODE)
+                        )
+                    }
                 }
             }
-        }
-        dispatcher.register(command)
+        })
     }
 
     @JvmStatic
-    private fun setMode(source: CommandSourceStack, targets: List<AquaPlayer>, mode: GameMode): Int {
-        var count = 0
+    private fun setGameMode(source: CommandSourceStack, targets: List<AquaPlayer>, mode: GameMode): Int {
         targets.forEach {
             it.updateGameMode(mode)
             notifyGameModeChanged(source, it, mode)
-            ++count
         }
-        return count
+        return targets.size
     }
 
     @JvmStatic
@@ -61,9 +50,11 @@ object GameModeCommand {
             CommandMessages.GAME_MODE_SELF.sendSuccess(source, mode, true)
             return
         }
+
         if (source.world.gameRules().getBoolean(GameRuleKeys.SEND_COMMAND_FEEDBACK)) {
-            source.sendSystemMessage(Component.translatable("gameMode.changed", Component.translatable(mode)))
+            player.sendSystemMessage(CommandMessages.GAME_MODE_CHANGED.build(mode))
         }
+
         CommandMessages.GAME_MODE_OTHER.sendSuccess(source, player.displayName, mode, true)
     }
 }
